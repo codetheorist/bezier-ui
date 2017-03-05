d32c83e7ef19645cd68db9a74b6cb6b464176ced
Alex Scott <alex.scott@tipsforthings.com>
2017-03-05 21:03:36 +0000

    Code refactoring

--- js/bezier.js
+++ js/bezier.js
@@ -54,105 +54,29 @@
           ctx = $('#bezier-ui-canvas-' + count)[0].getContext('2d'),
           curveBoundingBox = canvas.getBoundingClientRect();
 
-      console.log(ctx);
-      console.log(curveBoundingBox);
+
       count++;
-    });
+      console.log($(this).find('.bezier-ui-start-curve'));
 
-    $('.bezier-ui-start-curve, .bezier-ui-end-curve').each(function(){
+      // Store curve controls in variables
+      var $start = $($this).parent().siblings().find('.bezier-ui-start-curve');
+      var $end = $($this).parent().siblings().find('.bezier-ui-end-curve');
 
-      var $this = this;
-      var sx = $(this).children('.bezier-ui-start-curve').css('left'),
-          sy = $(this).children('.bezier-ui-start-curve').css('top'),
-          ex = $(this).children('.bezier-ui-end-curve').css('left'),
-          ey = $(this).children('.bezier-ui-end-curve').css('top');
-
-      $(this).draggable({
-        drag: function(e) {
-          var $left = ($($this).css('left').replace('px', '') / settings.size).toFixed(2),
-              $top = ($($this).css('top').replace('px', '') / settings.size).toFixed(2),
-              $values = $(this).siblings('.bezier-ui-value');
-
-          sx = $($values).find('.startX').text();
-          sy = $($values).find('.startY').text();
-          ex = $($values).find('.endX').text();
-          ey = $($values).find('.endY').text();
-          if ($(this).hasClass('bezier-ui-start-curve')) {
-            sx = $left;
-            sy = $top;
-            $($values).find('.startX').text(sx);
-            $($values).find('.startY').text(sy);
-            updateCanvas(sx, sy, ex, ey, $($this).siblings('.bezier-ui-canvas')[0].getContext("2d"));
-          }
-          else if ($(this).hasClass('bezier-ui-end-curve')) {
-            ex = $left;
-            ey = $top;
-            $($values).find('.endX').text(ex);
-            $($values).find('.endY').text(ey);
-            updateCanvas(sx, sy, ex, ey, $($this).siblings('.bezier-ui-canvas')[0].getContext("2d"));
-          }
-          var id = $(this).parent().data('bezierUiCoordinates');
-          $('[data-bezier="' + id + '"]').attr('value', 'cubic-bezier(' + sx + ', ' + sy + ', ' + ex + ', ' + ey + ')');
-        },
+      // Make start control draggable
+      $start.draggable({
         containment: 'parent'
       });
 
-      function updateCanvas(sx, sy, ex, ey, ctx) {
-        var ctx = ctx;
-        // Setup canvas stroke
-        ctx.lineWidth = 6;
-        ctx.strokeStyle = "black";
-
-        // Clear canvas
-        ctx.clearRect(0, 0, settings.size, settings.size);
-
-        // Begin canvas draw
-        ctx.beginPath();
-
-        // Move to bottom left of canvas
-        ctx.moveTo(0, settings.size);
-
-        // Create bezier to top right of canvas
-        ctx.bezierCurveTo(
-          sx * settings.size,
-          sy * settings.size,
-          ex * settings.size,
-          ey * settings.size,
-          settings.size, 0
-        );
-
-        // Actually draw curve on canvas
-        ctx.stroke();
-      }
-    });
-
-    $(document).ready(function() {
-      $('.coordinate-plane').each(function() {
-        var id = $(this).data('bezierUiCoordinates');
-        var sx = $(this).children('.bezier-ui-start-curve').css('left').replace('px', ''),
-            sy = $(this).children('.bezier-ui-start-curve').css('top').replace('px', ''),
-            ex = $(this).children('.bezier-ui-end-curve').css('left').replace('px', ''),
-            ey = $(this).children('.bezier-ui-end-curve').css('top').replace('px', '');
-        var $values = $(this).find('.bezier-ui-value');
-        $($values).find('.startX').text(sx);
-        $($values).find('.startY').text(sy);
-
-        $($values).find('.endX').text(ex);
-        $($values).find('.endY').text(ey);
-
-        console.log(sx, sy, ex, ey);
-        var ctx = $(this).children('.bezier-ui-canvas')[0].getContext("2d");
-        ctx.lineWidth = 6;
-        ctx.strokeStyle = "black";
-        ctx.clearRect(0, 0, settings.size, settings.size);
-        ctx.beginPath();
-        ctx.moveTo(0, settings.size);
-        ctx.bezierCurveTo(sx, sy, ex, ey, settings.size, 0);
-        ctx.stroke();
+      // Make end control draggable
+      $end.draggable({
+        containment: 'parent'
       });
+
     });
+
   }
-//   // Plugin defaults – added as a property on our plugin function.
+
+  // Plugin defaults – added as a property on our plugin function.
   $.fn.bezierFields.defaults = {
     appendHtml: false,
     selector: 'class',
